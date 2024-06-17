59aca8048d3ef03b1f77155425709af7083247be
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-06-17 23:26:40 +0300

    Создание приложений materials c моделями "Курс" и "урок", приложения users

---
+++ materials/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+from config import settings
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=255, verbose_name="Название", help_text="Введите название курса"
+    )
+    preview = models.ImageField(
+        upload_to="materials/img", verbose_name="Превью", **NULLABLE
+    )
+    description = models.TextField(verbose_name="Описание", **NULLABLE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=150, verbose_name="Название", help_text="Введите название урока"
+    )
+    description = models.TextField(verbose_name="Описание")
+    preview = models.ImageField(
+        upload_to="materials/", verbose_name="Превью", **NULLABLE
+    )
+    url = models.URLField(verbose_name="Ссылка на видео", **NULLABLE)
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name="Курс", related_name="lesson"
+    )
+
+    def __str__(self):
+        return f"Урок{self.name}, курс {self.course}"
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
