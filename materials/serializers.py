268e54fa4f6eaca36cbbe3a7152240ff002ce212
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-06-20 21:24:02 +0300

    добавил в сериализатор поле вывода количества уроков

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,23 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(ModelSerializer):
+    quantity_lessons = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+    def get_quantity_lessons(self, course):
+        return Lesson.objects.filter(course=course).count()
+
+
