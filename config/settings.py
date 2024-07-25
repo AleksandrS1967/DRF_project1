7f12f3fc0e9bc212b14593988592c9ada655a189
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-07-25 17:14:36 +0300

    Доработки для запуска контейнера с проектом

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,8 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "PORT": os.getenv("POSTGRES_PORT")
+        "PORT": os.getenv("PORT"),
+        "HOST": "postgres_container",
     }
 }
 
